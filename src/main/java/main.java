0fbb2d725de844606e03efd2f388dc3956b7251f
wangsiyuan <477944110@qq.com>
2022-01-22 19:38:48 +0800

    Git Idea test3  ---->hot-fix

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,5 +6,6 @@ public class main {
         System.out.println("test1..........");
         System.out.println("test2..........");
         System.out.println("test3..........");
+        System.out.println("test4..........");
     }
 }
